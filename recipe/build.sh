7e1301d4df7420b009798fc0a85d24e9a0a89edd
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-09-17 12:03:54 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,10 @@
 # https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
 # to use host python; requires that [binaries] section is last in meson_cross_file
 echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+cat ${CONDA_PREFIX}/meson_cross_file.txt
+
+if [[ $target_platform == "osx-arm64" ]]; then
+    sed -i.bak "s@\[properties\]@[properties]\nskip_sanity_check = true@g" ${CONDA_PREFIX}/meson_cross_file.txt
+fi
 
 python -m pip install . -vv --no-deps --no-build-isolation
