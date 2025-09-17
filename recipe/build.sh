9f2f62c54ff5c4be67507b5ffbc83f6d3f6aaaf6
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-09-17 14:00:07 +0200

    Build for Mac Os ARM (#25)
    * Update conda-forge.yml
    
    * Update meta.yaml
    
    * MNT: Re-rendered with conda-smithy 3.52.2 and conda-forge-pinning 2025.09.16.07.18.0
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Create build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update build.sh
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

---
+++ recipe/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir builddir
+
+# HACK: extend $CONDA_PREFIX/meson_cross_file that's created in
+# https://github.com/conda-forge/ctng-compiler-activation-feedstock/blob/main/recipe/activate-gcc.sh
+# https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
+# to use host python; requires that [binaries] section is last in meson_cross_file
+echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+
+# -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+$PYTHON -m build -w -n -x \
+    -Cbuilddir=builddir \
+    -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
+    || (cat builddir/meson-logs/meson-log.txt && exit 1)
+
+$PYTHON -m pip install dist/matscipy*.whl
